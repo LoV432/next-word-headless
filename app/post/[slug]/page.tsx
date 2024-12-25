28436a5d1e207bd4a77825fdf7da03663bb10c28
LoV432 <monib619@gmail.com>
2024-12-26 00:13:27 +0500

    update post/comment layout

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { GetPostQuery } from '@/gql/graphql';
 import getPost from '@/lib/wordpress/getPost';
 import Image from 'next/image';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 type CommentsType = NonNullable<
 	NonNullable<GetPostQuery['post']>['comments']
@@ -17,7 +18,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
 	}
 	return (
 		<main className="container mx-auto grid max-w-[800px] grid-cols-1 place-items-start gap-6 px-4 py-8">
-			<h1 className="text-3xl font-bold">{post.title}</h1>
+			<h1 className="text-5xl font-bold">{post.title}</h1>
 			<div className="h-[300px] w-full">
 				<Image
 					src={
@@ -37,14 +38,14 @@ export default async function Post({ params }: { params: { slug: string } }) {
 				{new Date(post.date || '').toLocaleDateString()}
 			</div>
 			<article
-				className="prose lg:prose-lg min-h-[300px] max-w-full"
+				className="prose lg:prose-lg prose-img:border-2 prose-img:rounded prose-img:border-black min-h-[300px] max-w-full pb-32"
 				// TODO: Figure out if this is safe or not
 				dangerouslySetInnerHTML={{
 					__html: post.content || ''
 				}}
 			></article>
 			<div className="flex w-full flex-col gap-5 border-t border-gray-200 pt-4">
-				<h2 className="text-xl font-bold">
+				<h2 className="text-xl font-bold text-gray-600">
 					Comments ({post.commentCount || 0})
 				</h2>
 				<Comments comments={post.comments?.nodes || []} />
@@ -55,23 +56,30 @@ export default async function Post({ params }: { params: { slug: string } }) {
 
 function Comments({ comments }: { comments: CommentsType }) {
 	return (
-		<div className="mt-auto text-sm text-gray-500">
+		<div className="mt-auto flex flex-col gap-5 text-gray-500">
 			{comments.map((comment) => (
-				<div
-					key={comment.date}
-					className="rounded border border-gray-200 p-5 pb-4"
-				>
-					<div className="mb-2 text-gray-600">
-						By {comment.author?.name} on{' '}
-						{new Date(comment.date || '').toLocaleDateString()}
+				<div className="flex flex-col gap-2 rounded border border-gray-200 p-5">
+					<div className="flex items-center gap-2">
+						<Avatar>
+							<AvatarImage
+								src={`https://api.dicebear.com/6.x/bottts/svg?seed=${comment.author?.name}`}
+							/>
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+						<div className="mb-2 text-gray-600">
+							By {comment.author?.name} on{' '}
+							{new Date(comment.date || '').toLocaleDateString()}
+						</div>
 					</div>
-					<div
-						className="prose lg:prose-lg max-w-full"
-						// TODO: Figure out if this is safe or not
-						dangerouslySetInnerHTML={{
-							__html: comment.content || ''
-						}}
-					></div>
+					<div key={comment.date}>
+						<div
+							className="prose prose-base max-w-full"
+							// TODO: Figure out if this is safe or not
+							dangerouslySetInnerHTML={{
+								__html: comment.content || ''
+							}}
+						></div>
+					</div>
 				</div>
 			))}
 		</div>
